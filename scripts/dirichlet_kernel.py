fe223c88815db61eb1fe96f90aa38280b8bd1653
jxm-math <juanjomadrigal326@gmail.com>
2019-01-27 15:46:20 +0100

    Fix: dirichlet kernel range

--- scripts/dirichlet_kernel.py
+++ scripts/dirichlet_kernel.py
@@ -1,3 +1,3 @@
 t = var('t')
-dirichlet_kernel = [plot(sum([cos(i*t) for i in range(-n,n)]), (t,-pi,pi), ymin=-10, ymax=10, thickness=3, rgbcolor=(0.1,0.4,0.9)) for n in range(0,20)]
+dirichlet_kernel = [plot(sum([cos(i*t) for i in range(-n,n+1)]), (t,-pi,pi), ymin=-10, ymax=10, thickness=3, rgbcolor=(0.1,0.4,0.9)) for n in range(0,20)]
 animate(dirichlet_kernel)
